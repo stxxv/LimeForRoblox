d4a73e51d4393c89cfafc1d22a41a123d685d3b5
stav <286843031+stxxv@users.noreply.github.com>
2026-09-19 16:46:26 +1000

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,8 +1,8 @@
 repeat task.wait() until game:IsLoaded()
 if game.PlaceId == 11630038968 or game.PlaceId == 10810646982 or game.PlaceId == 139566161526375 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/not-hm/LimeForRoblox/refs/heads/main/bridge_duel.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/stxxv/LimeForRoblox/refs/heads/main/bridge_duel.lua"))()
 elseif game.PlaceId == 123941384302059 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/not-hm/LimeForRoblox/refs/heads/main/bridge_sky.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/stxxv/LimeForRoblox/refs/heads/main/bridge_sky.lua"))()
 else
 	game:GetService("Chat"):Chat(game.Players.LocalPlayer.Character:WaitForChild("Head"), "I love eating AquaVClip feces")
 end
